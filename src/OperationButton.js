3be2ea4520667d70bae85b81adec3052c4e13093
JonnyRiver <jonnyriver6@gmail.com>
2023-07-13 15:35:01 -0400

    finished

---
+++ src/OperationButton.js
@@ -0,0 +1,12 @@
+
+import { ACTIONS } from "./App"
+
+export default function OperationButtonButton({ dispatch, operation }) {
+    return (
+        <button
+            onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}
+        >
+            {operation}
+        </button>
+    )
+}
